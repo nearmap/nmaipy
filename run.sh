80c402ec42ec9ce26819021349eeb7c547ec5141
mbewley <mbewley@users.noreply.github.com>
2024-11-21 15:49:22 +1100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 export PYTHONPATH=.
 ulimit -n 32000 # Increase ability to leave open files, for running many workers (e.g. 32+).
-python /home/jovyan/Development/datascience/nmaipy/scripts/ai_offline_parcel.py \
-    --parcel-dir "data/parcels" \
+python /home/jovyan/Development/datascience/nmaipy/exporter.py \
+    --aoi-file "data/example.geojson" \
     --output-dir "data/outputs" \
     --country us \
-    --workers 4 \
+    --processes 4 \
     --system-version-prefix "gen6-" \
     --packs building vegetation \
     --include-parcel-geometry \
