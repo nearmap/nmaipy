b739577bc21c28bf26d2f596e9a085372b9abd4e
Michael Bewley <michael.bewley@nearmap.com>
2024-11-21 04:58:09 +0000

    [TRO-3533] Fixed run.sh example

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 export PYTHONPATH=.
 ulimit -n 32000 # Increase ability to leave open files, for running many workers (e.g. 32+).
-python /home/jovyan/Development/datascience/nmaipy/exporter.py \
-    --aoi-file "data/example.geojson" \
+python nmaipy/exporter.py \
+    --aoi-file "data/sydney-cbd-test.geojson" \
     --output-dir "data/outputs" \
-    --country us \
+    --country au \
     --processes 4 \
     --system-version-prefix "gen6-" \
     --packs building vegetation \
