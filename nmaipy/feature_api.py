e977f31c7e6dd563055a7cd4f13b807dca0d441b
Michael Bewley <michael.bewley@nearmap.com>
2025-03-06 23:17:32 +0000

    [TRO-3679] Fixed filtering out of small buildings with less than 50% intersection.

--- nmaipy/feature_api.py
+++ nmaipy/feature_api.py
@@ -841,7 +841,7 @@ class FeatureApi:
         Check whether the link contains the location marker flag, and add it if not present.
         """
         location_marker_string = "?locationMarker"
-        if not location_marker_string not in link:
+        if location_marker_string not in link:
             return link + location_marker_string
         else:
             return link
