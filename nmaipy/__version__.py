54a946b25a59ccf17121cebc3a0a0d2888fdd790
mbewley <michael.bewley@nearmap.com>
2026-04-22 17:51:08 +1000

    5.0.0 alpha bump

--- nmaipy/__version__.py
+++ nmaipy/__version__.py
@@ -1,6 +1,6 @@
 """Version information for nmaipy."""
 
-__version__ = "4.7.0"
+__version__ = "5.0.0a1"
 __version_info__ = tuple(
     int(i) for i in __version__.replace("a", ".").replace("b", ".").replace("rc", ".").split(".")[:3]
 )
