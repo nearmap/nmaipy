73568f385c7d8f9de2082cd10acbd253b483c055
mbewley <michael.bewley@nearmap.com>
2026-07-01 22:09:22 +1000

    Release version 5.1.0

--- nmaipy/__version__.py
+++ nmaipy/__version__.py
@@ -1,6 +1,6 @@
 """Version information for nmaipy."""
 
-__version__ = "5.1.0a3"
+__version__ = "5.1.0"
 __version_info__ = tuple(
     int(i) for i in __version__.replace("a", ".").replace("b", ".").replace("rc", ".").split(".")[:3]
 )
